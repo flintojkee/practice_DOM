e9ef7600f94afbf2e9ac5733117f72d906cc4ff5
DenysVasylenko <32224385+flintojkee@users.noreply.github.com>
2018-07-10 07:37:45 +0300

    deleteButton

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,20 @@
 const dataUrl = "https://api.myjson.com/bins/152f9j";
 let cards = [];
 let numberOfCards = 0;
+let filteredCards;
 const cardsHtml = document.getElementById("cards");
 const tagsHtml = document.getElementById("tags");
 const getBackButton = document.getElementById("getBackButton");
+const deleteCardButtons = document.getElementsByClassName("deleteButton");
 const inputSearch = document.getElementById('inputSearch');
 window.onload = () => {
     fetch(dataUrl)
         .then((resp) => resp.json())
         .then(function (data) {
             cards = data.data;
-            showCards();
+            filteredCards = cards;
+            showCards(filteredCards);
+            numberOfCards+=10;
             showTags();
         })
         .catch(err =>{
@@ -18,34 +22,35 @@ window.onload = () => {
         });
 };
 
-function showCards(){
 
-    for(let i = numberOfCards; i < numberOfCards+10; i++){
-        let cardTags = "";
-        for(let j = 0; j < cards[i].tags.length; j++){
-            cardTags+="<div class='tag'>"+cards[i].tags[j]+"</div>"
-        }
+function showCards(cardsToShow){
+
+    for(let i = numberOfCards; i < numberOfCards+10 && i < cardsToShow.length; ++i){
+        console.log(i);
         let card =
             "<div class='card' id="+i+">" +
                 "<div class='container'>" +
-                    "<button class=\"deleteButton\"><i class=\"fa fa-close\"></i></button>"+
-                    "<img src="+cards[i].image +" alt=\"Avatar\">"+
-                    "<div class='title'>"+cards[i].title+"</div>" +
-                    "<div class='desc'>"+cards[i].description+"</div>" +
-                    "<div class='date'>"+cards[i].createdAt+"</div>" +
-                    "<div class='tags'>"+cardTags+
+                    "<button class=\"deleteButton\" onclick='deleteCard(event)'><i class=\"fa fa-close\"></i></button>"+
+                    "<img src="+cardsToShow[i].image +" alt=\"Avatar\">"+
+                    "<div class='title'>"+cardsToShow[i].title+"</div>" +
+                    "<div class='desc'>"+cardsToShow[i].description+"</div>" +
+                    "<div class='date'>"+cardsToShow[i].createdAt+"</div>" +
+                    "<div class='tags'>"+cardsToShow[i].tags+
             "</div></div></div>";
         cardsHtml.innerHTML += card;
     }
 }
-console.log(window.pageYOffset);
+
+
+
 window.onscroll = function () {
     var pageHeight=document.documentElement.offsetHeight,
         windowHeight=window.innerHeight,
         scrollPosition=window.scrollY || window.pageYOffset || document.body.scrollTop + (document.documentElement && document.documentElement.scrollTop || 0);
     if (pageHeight <= windowHeight+scrollPosition && numberOfCards < 50) {
+        showCards(cards);
         numberOfCards+=10;
-        showCards();
+        console.log(numberOfCards);
     }
 };
 
@@ -66,27 +71,52 @@ function scrollTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
     cardsHtml.innerHTML = "";
-    showCards();
+    numberOfCards = 0;
+    showCards(filteredCards);
 }
 
 getBackButton.addEventListener('click', scrollTop);
 
 function search() {
-    let filter;
-    filter = inputSearch.value.toUpperCase();
-    // Loop through all list items, and hide those who don't match the search query
-
-    for (i = 0; i < numberOfCards+10; i++) {
-       // console.log(cards[i]);
-        title = cards[i].title;
-        c = document.getElementById(i);
+    cardsHtml.innerHTML = "";
+    let filter = inputSearch.value.toUpperCase();
+    let searchCards = filteredCards.slice();
+    for (i = 0; i < filteredCards.length; i++) {
+        title = filteredCards[i].title;
         if (title.toUpperCase().indexOf(filter) > -1) {
-            c.style.display = "";
+
         } else {
-            c.style.display = "none";
+            searchCards.splice(searchCards.indexOf(filteredCards[i]),1);
         }
+
     }
+    filteredCards = searchCards;
+    numberOfCards = 0;
+    showCards(filteredCards);
+
 }
+
+for (let i = 0; i < deleteCardButtons.length; i++) {
+    deleteCardButtons[i].addEventListener('click', deleteCard);
+}
+
+function deleteCard() {
+    cardsHtml.innerHTML = "";
+    let card = event.target.parentNode.parentNode;
+    let filter = card.getElementsByClassName("date")[0].innerHTML;
+    let cardsLeft = filteredCards.slice();
+    for (i = 0; i < filteredCards.length; i++) {
+        date = filteredCards[i].createdAt;
+        if (date.indexOf(filter) > -1) {
+            cardsLeft.splice(cardsLeft.indexOf(filteredCards[i]),1);
+        } 
+
+    }
+    filteredCards = cardsLeft;
+    numberOfCards = 0;
+    showCards(filteredCards);
+}
+
 
 inputSearch.addEventListener("input", search);
 console.log(inputSearch.value);
